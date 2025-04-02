5b26fc9e7911f9918bf136760a5567e50cb8e18a
CamiloTello002 <camilojtello5@gmail.com>
2025-04-02 11:33:38 -0500

    feat: install lazydocker plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -22,20 +22,21 @@ require('lazy').setup({
   require 'plugins/telescope',
   require 'plugins/conform',
   require 'plugins/nvim-cmp',
-  require 'plugins/nvim-treesitter'
-    -- LSP Plugins
-    {
-      -- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
-      -- used for completion, annotations and signatures of Neovim apis
-      'folke/lazydev.nvim',
-      ft = 'lua',
-      opts = {
-        library = {
-          -- Load luvit types when the `vim.uv` word is found
-          { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
-        },
+  require 'plugins/nvim-treesitter',
+  require 'plugins/lazydocker',
+  -- LSP Plugins
+  {
+    -- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
+    -- used for completion, annotations and signatures of Neovim apis
+    'folke/lazydev.nvim',
+    ft = 'lua',
+    opts = {
+      library = {
+        -- Load luvit types when the `vim.uv` word is found
+        { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
       },
     },
+  },
   {
     -- Main LSP Configuration
     'neovim/nvim-lspconfig',

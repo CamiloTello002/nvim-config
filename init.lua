998f76f94692d1ec39bd20fbf0a848381799b066
CamiloTello002 <camilojtello5@gmail.com>
2025-04-02 10:33:30 -0500

    refactor: import options from core.options

--- init.lua
+++ init.lua
@@ -1,31 +1,7 @@
 -- This config file has defaults you'd expect in an average
+-- text editor
 
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
-vim.g.have_nerd_font = false
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.mouse = 'a'
-vim.opt.showmode = false
-vim.schedule(function()
-  vim.opt.clipboard = 'unnamedplus'
-end)
-vim.opt.breakindent = true
-vim.opt.undofile = true
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-vim.opt.signcolumn = 'yes'
-vim.opt.updatetime = 250
-vim.opt.timeoutlen = 300
-vim.opt.splitright = true
-vim.opt.splitbelow = true
-vim.opt.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
-vim.opt.inccommand = 'split'
-vim.opt.cursorline = true
-vim.opt.scrolloff = 10
-vim.opt.confirm = true
-vim.opt.laststatus = 3
+require 'core.options'
 
 -- [[ Keymaps ]]
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
@@ -151,6 +127,14 @@ require('lazy').setup({
         },
         ft = { 'markdown', 'Avante' },
       },
+    },
+  },
+  -- toggle term
+  {
+    'akinsho/toggleterm.nvim',
+    version = '*',
+    opts = {
+      open_mapping = [[<M-i>]],
     },
   },
   {
